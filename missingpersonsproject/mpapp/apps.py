a9c194a920e1eded332c14c584a354b4ae4654ef
Ozarkies97 <120748604+Ozarkies97@users.noreply.github.com>
2023-04-15 17:09:53 -0600

    Update apps.py

--- missingpersonsproject/mpapp/apps.py
+++ missingpersonsproject/mpapp/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
 from django.urls import path
 from .views import indexPageView, missingpersonstable.html,
+individualpersonpageview
+
+urlpatterns = [  
+    path("showPerson/", showStudentsPageView, 
+name="showStudents"), 
+    path("<str:sName>/", displayPageView, name="displayScreen"), 
+    path("", indexPageView, name="index"),
+] 
 
 class MpappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
